9e163f8b560f7b0e6124facc9d7552de788838de
Carlos Angarita <carlosreneas@ufps.edu.co>
2024-10-28 10:50:11 -0500

    Inclusión relación MANYTOMANY

---
+++ src/main/java/co/edu/ufps/entities/Funcion.java
@@ -0,0 +1,29 @@
+package co.edu.ufps.entities;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="funcion")
+public class Funcion {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String descripcion;
+	
+	@ManyToMany(mappedBy = "funciones")
+    List<TipoPersona> tipoPersonas;
+
+}
--- src/main/java/co/edu/ufps/entities/TipoPersona.java
+++ src/main/java/co/edu/ufps/entities/TipoPersona.java
@@ -9,8 +9,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.JoinColumn;
 import lombok.Data;
 
 @Entity
@@ -27,5 +30,12 @@ public class TipoPersona {
 	@OneToMany(mappedBy = "tipoPersona", cascade= CascadeType.ALL)
 	@JsonIgnore
 	List<Personal> personals;
+	
+	@ManyToMany
+	@JoinTable(
+			  name = "tipo_personal_funcion", 
+			  joinColumns = @JoinColumn(name = "tipo_persona_id"), 
+			  inverseJoinColumns = @JoinColumn(name = "funcion_id"))
+    List<Funcion> funciones;
 
 }
