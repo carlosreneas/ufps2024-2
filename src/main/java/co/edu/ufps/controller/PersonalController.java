0d0f09efe24a7d846db09a43aedba29074fdc36b
docente <docente@172.18.22.211>
2024-11-01 10:37:37 -0500

    Ajustes

--- src/main/java/co/edu/ufps/controller/PersonalController.java
+++ src/main/java/co/edu/ufps/controller/PersonalController.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import co.edu.ufps.entities.Funcion;
 import co.edu.ufps.entities.Personal;
 import co.edu.ufps.repositories.PersonalRepository;
 import co.edu.ufps.services.PersonalService;
@@ -21,6 +24,13 @@ public class PersonalController {
 	@GetMapping()
 	public List<Personal> list() {
 		return personalService.list();
+	}
+	
+	@PostMapping()
+	public Personal create(@RequestBody Personal personal) {
+		
+		Personal nuevaPersonal = personalService.create(personal);
+		return nuevaPersonal;
 	}
 
 }
--- src/main/java/co/edu/ufps/services/PersonalService.java
+++ src/main/java/co/edu/ufps/services/PersonalService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import co.edu.ufps.entities.Funcion;
 import co.edu.ufps.entities.Personal;
 import co.edu.ufps.repositories.PersonalRepository;
 
@@ -16,6 +17,11 @@ public class PersonalService {
 
 	public List<Personal> list() {
 		return personalRepository.findAll();
+	}
+	
+	public Personal create(Personal personal) {
+		// TODO Auto-generated method stub
+		return personalRepository.save(personal);
 	}
 	
 }
